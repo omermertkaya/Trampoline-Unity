b175868d21e6f0594b8ebef13dd5d62a34b11929
Omer Mert KAYA <l1421012018@stud.sdu.edu.tr>
2019-04-10 19:12:31 +0300

    Create Trampoline.cs

---
+++ Trampoline.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+
+    Animator anim;
+    public GameObject bouncer;
+    public Vector2 velocity;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        //Debug.Log("Stay Tamam");
+        anim.SetBool("isStepped", true);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("EnterTamam");
+
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("exit tamam");
+            anim.SetBool("isStepped", false);
+           
+
+        }
+    }
+    void Jump()
+    {
+        bouncer.GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+
+}
+
+
